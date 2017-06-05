8d8276c420159bb94f1615abb3c0820187333627
James Burnside <jcburnside97@gmail.com>
2017-06-05 15:49:04 -0400

    Initial Commit

---
+++ javascript.js
@@ -0,0 +1,14 @@
+var loop=function(color){
+	if(++color==255**3){
+		color=0;
+	}
+
+}
+var toHex=function(numb){
+	let firstSet=numb%(255**2);
+	let secSet=(numb-firstSet)%255;
+	let LastSet=(numb-firstSet-secSet);
+	console.log("#"+firstSet+" "+secSet+" "+lastSet);
+}
+console.log(document.body.style);
+//toHex()
